ae24b57f41bb57eb72df79b7455ea626d79e5022
Gibby <jawshanderson990@gmail.com>
2024-05-15 11:19:56 -0700

    Created config for global variables, will use this later.

---
+++ gooseplayer2/src/main/java/com/gooseplayer2/Config.java
@@ -0,0 +1,7 @@
+package com.gooseplayer2;
+
+import java.io.File;
+
+public class Config {
+    public static final String LIBRARY_PATH = System.getProperty("user.dir") + File.separator + "Library";
+}
