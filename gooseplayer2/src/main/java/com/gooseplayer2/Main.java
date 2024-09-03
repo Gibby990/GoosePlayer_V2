d658dd4949fb348fcafd4b2468575f271cf37601
Joshua <jawshanderson990@gmail.com>
2024-09-03 15:35:56 -0500

    Updated TODO

--- gooseplayer2/src/main/java/com/gooseplayer2/Main.java
+++ gooseplayer2/src/main/java/com/gooseplayer2/Main.java
@@ -8,7 +8,7 @@ import javazoom.jl.decoder.JavaLayerException;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> { //TODO: add JRE to project 
+        SwingUtilities.invokeLater(() -> { //TODO: Make Project run off of the JRE
             try {
                 new MainFrame();
             } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | JavaLayerException e) {
