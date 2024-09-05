9c0d25e5d8546993369604b9fca3aaca554d50df
Joshua <jawshanderson990@gmail.com>
2024-09-05 15:48:02 -0500

    Cleaned up

--- gooseplayer2/src/main/java/com/gooseplayer2/Packages/DropFileHandler.java
+++ gooseplayer2/src/main/java/com/gooseplayer2/Packages/DropFileHandler.java
@@ -9,7 +9,6 @@ import com.gooseplayer2.JPanels.*;
 
 public class DropFileHandler extends TransferHandler {
     MusicPlayer player;
-    // Assuming FilePanel is a type of JComponent that represents your library panel
     JComponent filePanel;
 
     public DropFileHandler(MusicPlayer player, JComponent filePanel) {
@@ -25,9 +24,7 @@ public class DropFileHandler extends TransferHandler {
 
         support.setDropAction(COPY); 
 
-        // Check if the drop is coming from the FilePanel
         if (support.getComponent() == filePanel) {
-            // Here you can add additional checks or preparations
         }
 
         return support.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
@@ -45,7 +42,6 @@ public class DropFileHandler extends TransferHandler {
             @SuppressWarnings("unchecked")
             List<File> files = (List<File>) transferable.getTransferData(DataFlavor.javaFileListFlavor);
 
-            // Add files to the specific player's queue
             player.addFilesToTree(files);
 
             return true;
