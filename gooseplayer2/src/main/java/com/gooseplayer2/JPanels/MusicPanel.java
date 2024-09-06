56ad8d44497200a3b1b9e047e13edfe8ec183bd1
Joshua <jawshanderson990@gmail.com>
2024-09-06 09:24:41 -0500

    It is done

--- gooseplayer2/src/main/java/com/gooseplayer2/JPanels/MusicPanel.java
+++ gooseplayer2/src/main/java/com/gooseplayer2/JPanels/MusicPanel.java
@@ -13,7 +13,7 @@ public class MusicPanel extends JPanel {
     private GridBagConstraints gbc;
     private Border outline;
     private FilePanel filePanel;
-    private MusicPlayer player1, player2, player3; // TODO: Add Fading
+    private MusicPlayer player1, player2, player3;
 
     public MusicPanel() throws UnsupportedAudioFileException, IOException, LineUnavailableException, JavaLayerException {
 
